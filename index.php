1e9aaf5f5124e76d5804f0338eea6e73352cbd5b
Kamlesh Mathe <kamlesh.mathe@ril.com>
2022-12-30 12:14:25 +0530

    code added

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>PHP Test app</title>
+</head>
+
+<body>
+    <h2>Welcome to PHP Test app</h2>
+    <h3>PHP Version: <?php phpversion(); ?></h3>
+</body>
+
+</html>
